3653b731c3b36d2ecb54796f7f15cb2ec326963b
Aleksbgbg <kozichka01@abv.bg>
2018-06-16 22:22:30 +0100

    [Main] Add SFML main loop and game mainloop call.

--- MMO RPG/Main.cpp
+++ MMO RPG/Main.cpp
@@ -1,4 +1,33 @@
+#include <SFML/Graphics.hpp>
+
+#include "Game.h"
+
 int main()
 {
+	sf::RenderWindow window{ sf::VideoMode{ 1366, 768 }, "MMO RPG" };
+
+	Game game{ window };
+
+	while (window.isOpen())
+	{
+		{
+			sf::Event event;
+
+			while (window.pollEvent(event))
+			{
+				if (event.type == sf::Event::Closed)
+				{
+					window.close();
+				}
+			}
+		}
+
+		window.clear();
+
+		game.Main();
+
+		window.display();
+	}
+
 	return 0;
 }
