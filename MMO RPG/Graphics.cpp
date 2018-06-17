86271f3d5bac60d9480c35a1d12b381214a9312f
Aleksbgbg <kozichka01@abv.bg>
2018-06-17 13:34:00 +0100

    [Graphics] Add graphics class for managing drawable objects and rendering.

---
+++ MMO RPG/Graphics.cpp
@@ -0,0 +1,35 @@
+#include "Graphics.h"
+
+Graphics::Graphics(sf::RenderWindow& window)
+	:
+	_window{ window }
+{
+}
+
+void Graphics::Begin() const
+{
+	_window.clear();
+}
+
+void Graphics::Render() const
+{
+	for (const sf::Drawable* const drawable : _buffer)
+	{
+		_window.draw(*drawable);
+	}
+}
+
+void Graphics::End() const
+{
+	_window.display();
+}
+
+void Graphics::Add(sf::Drawable& drawable)
+{
+	_buffer.push_back(&drawable);
+}
+
+void Graphics::Remove(sf::Drawable& drawable)
+{
+	_buffer.erase(std::remove(_buffer.begin(), _buffer.end(), &drawable), _buffer.end());
+}
---
+++ MMO RPG/Graphics.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <SFML/Graphics/Drawable.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+
+#include <vector>
+
+class Graphics
+{
+public:
+	Graphics(sf::RenderWindow& window);
+
+public:
+	void Begin() const;
+	void Render() const;
+	void End() const;
+
+public:
+	void Add(sf::Drawable& drawable);
+	void Remove(sf::Drawable& drawable);
+
+public:
+	static constexpr unsigned int ScreenWidth = 1366;
+	static constexpr unsigned int ScreenHeight = 768;
+
+private:
+	sf::RenderWindow& _window;
+	std::vector<sf::Drawable*> _buffer;
+};
