843ab7e7058103bc83a2ddeb1b0acc14d27b5c53
Aleksbgbg <kozichka01@abv.bg>
2018-06-25 09:09:10 +0100

    [Game] Move camera with arrow keys.

--- MMO RPG/Game.cpp
+++ MMO RPG/Game.cpp
@@ -47,6 +47,33 @@ void Game::UpdateModel()
 	{
 		camera.SwitchMode(Camera::Mode::SemiFree);
 	}
+
+	{
+		sf::Vector2f cameraMovement;
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
+		{
+			cameraMovement.x = -1;
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
+		{
+			cameraMovement.x = 1;
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up))
+		{
+			cameraMovement.y = -1;
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down))
+		{
+			cameraMovement.y = 1;
+		}
+
+		if (!(cameraMovement.x == 0 && cameraMovement.y == 0))
+		{
+			camera.MoveBy(cameraMovement);
+		}
+	}
 }
 
 void Game::ComposeFrame()
