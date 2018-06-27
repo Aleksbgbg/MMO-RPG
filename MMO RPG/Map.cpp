60d72cd4933a8cea8e163f326f10a12b621210fc
Aleksbgbg <kozichka01@abv.bg>
2018-06-27 16:59:33 +0100

    [Map] Compress map file, and use Tile class.

--- MMO RPG/Map.cpp
+++ MMO RPG/Map.cpp
@@ -3,7 +3,6 @@
 #include "json.hpp"
 
 #include <fstream>
-#include <string>
 
 #include "Graphics.h"
 #include "Map.h"
@@ -12,161 +11,63 @@
 using json = nlohmann::json;
 
 Map::Map()
-	:
-	texture{ TextureManager::Get("Map") }
 {
-	// TODO: Remove hardcoded
-	ParseFileToJson("Map.json");
-	PopulateSpritePositions();
-	AddAllSprites();
-}
+	json mapDetails;
 
-void Map::AddTexture(const sf::Texture& texture)
-{
-	textures.push_back(texture);
+	{
+		std::ifstream input{ "Map.json" };
+
+		if (!input.is_open())
+		{
+			throw std::runtime_error{ "Could not open file Map.json" };
+		}
+
+		mapDetails = json::parse(input);
+	}
+
+	mapTiles = sf::Vector2i{ mapDetails["width"], mapDetails["height"] };
+
+	tileDimensions = sf::Vector2i{ mapDetails["tilewidth"], mapDetails["tileheight"] };
+
+	const sf::Texture& mapTexture = TextureManager::Get("Map");
+
+	const sf::Vector2i tileCount{ static_cast<int>(mapTexture.getSize().x) / tileDimensions.x, static_cast<int>(mapTexture.getSize().y) / tileDimensions.y };
+
+	for (const json& layer : mapDetails["layers"])
+	{
+		const json& layerData = layer["data"];
+
+		int index = -1;
+
+		for (int y = 0; y < mapTiles.y; ++y)
+		{
+			for (int x = 0; x < mapTiles.x; ++x)
+			{
+				int tileIndex = layerData[++index];
+
+				if (tileIndex > 0)
+				{
+					tileIndex -= 1;
+
+					const int tileColumn = tileIndex % tileCount.x;
+					const int tileRow = tileIndex / tileCount.x;
+
+					tiles.emplace_back(mapTexture, sf::Vector2i{ tileColumn * tileDimensions.x, tileRow * tileDimensions.y }, sf::Vector2f{ static_cast<float>(x * tileDimensions.x), static_cast<float>(y * tileDimensions.y) }, Scale, tileDimensions);
+				}
+			}
+		}
+	}
 }
 
 void Map::Draw(const Graphics& gfx)
 {
-	for (const sf::Sprite& sprite : sprites)
+	for (const Tile& tile : tiles)
 	{
-		gfx.Draw(sprite);
+		tile.Draw(gfx);
 	}
-}
-
-void Map::AddSprite(const int index, const float x, const float y, const int width, const int height)
-{
-	sf::Sprite sprite{ };
-
-	sprite.setTexture(TextureManager::Get("Map"));
-	sprite.setTextureRect(sf::IntRect{ spritePositions[index].x, spritePositions[index].y, width, height });
-	sprite.setScale(2.0f, 2.0f);
-	sprite.setPosition(x, y);
-
-	sprites.push_back(sprite);
-}
-
-nlohmann::json Map::GetJsonData() const
-{
-	return jsonData;
-}
-
-nlohmann::json Map::GetAllLayers() const
-{
-	return jsonData["layers"];
-}
-
-nlohmann::json Map::GetLayerData(int layerId) const
-{
-	return GetAllLayers()["layers"]["data"];
 }
 
 sf::Vector2i Map::GetDimensions() const
 {
-	return sf::Vector2i(GetTileAmount().x * GetTileSize().x, GetTileAmount().y * GetTileSize().y);
-}
-
-sf::Vector2i Map::GetTileAmount() const
-{
-	return sf::Vector2i(jsonData["width"], jsonData["height"]);
-}
-
-sf::Vector2i Map::GetTileSize() const
-{
-	/*
-	// So we can apply scaling meme
-	// TODO: Rename?!?
-	const int actualWidth = static_cast<int>(jsonData["tilewidth"]) * sprites.back().getScale().x;
-	const int actualHeight = static_cast<int>(jsonData["tileheight"]) * sprites.back().getScale().y;
-	*/
-	const int actualHeight = static_cast<int>(static_cast<int>(jsonData["tileheight"]) * sprites.back().getScale().y);
-
-
-	// TODO: Sort out loading order issue and remove hard coded value
-	return sf::Vector2i(static_cast<int>(jsonData["tilewidth"]) * 2, static_cast<int>(jsonData["tileheight"])* 2);
-}
-
-void Map::AddAllSprites()
-{
-	int screenWidth = Graphics::ScreenWidth;
-	int screenHeight = Graphics::ScreenWidth;
-
-	// This is post scaling so don't multiply it or do any other magic spells
-	// TODO: Probably change this from direct access
-	const json tileWidth = jsonData["tilewidth"];
-	const json tileHeight = jsonData["tileheight"];
-
-	const json layers = GetAllLayers();
-	const int totalWidth = GetTileAmount().x;
-
-	// TODO: Probably change this from direct access
-	for (json layer : layers)
-	{
-		json data = layer["data"];
-		
-		int tileX = 0, tileY = 0;
-
-		for (int tileId : data)
-		{
-			// Gotta -1 because the JSON data reserves 0 for no sprite and as such minusing one gets us back on track because our Vector stores from 0
-			// TODO: Maybe make the this clearer somehow
-			if(tileId > 0)
-			{
-				tileId -= 1;
-				// This must be unscaled... this is maybe a crap thing
-				AddSprite(tileId, static_cast<float>(tileX), static_cast<float>(tileY), tileWidth, tileHeight);
-			}
-
-			// TODO: Look into why tileWidth above is wrong
-			// SOLUTION: Was scaling elsewhere
-			tileX += 32;
-			
-			// TODO: Make sure hardcdoded val is changed along with other TOOD's
-			if (tileX == GetDimensions().x)
-			{
-				tileX = 0;
-				// TODO: Look into why tileHeight above is wrong
-				// SOLUTION: Was scaling elsewhere
-				tileY += 32;
-			}
-		}
-	}
-}
-
-void Map::ParseFileToJson(const std::string& filename)
-{
-	std::ifstream input{ filename };
-
-	if (!input.is_open())
-	{
-		throw std::runtime_error{ "Could not open file " + filename };
-	}
-
-	jsonData = json::parse(input);
-}
-
-void Map::PopulateSpritePositions()
-{
-	// TODO: Maybe move this into it's own method
-	const int tilesHorizontal = texture.getSize().x / 16;
-	const int tilesVertical = texture.getSize().y / 16;
-	const int tilesTotal = tilesHorizontal * tilesVertical;
-
-	unsigned int xPos = 0;
-	unsigned int yPos = 0;
-
-	for (int i = 0; i < tilesTotal; i++)
-	{
-		spritePositions.emplace_back(xPos, yPos);
-
-		xPos += 16;
-
-
-		// TODO: Remove hardcoded number
-		if (xPos == texture.getSize().x)
-		{
-			xPos = 0;
-			yPos += 16;
-		}
-	}
+	return sf::Vector2i{ static_cast<int>(mapTiles.x * tileDimensions.x * Scale), static_cast<int>(mapTiles.y * tileDimensions.y * Scale) };
 }
--- MMO RPG/Map.h
+++ MMO RPG/Map.h
@@ -2,11 +2,9 @@
 
 #include <SFML/Graphics.hpp>
 
-#include "json.hpp"
-
-#include <string>
 #include <vector>
 
+#include "Tile.h"
 
 class Graphics;
 
@@ -16,37 +14,17 @@ public:
 	Map();
 
 public:
-	void AddTexture(const sf::Texture& texture);
-	void AddAllSprites();
 	void Draw(const Graphics& gfx);
-	void AddSprite(const int index, const float x, const float y, const int width, const int height);
 
 public:
-	nlohmann::json GetJsonData() const;
-	nlohmann::json GetAllLayers() const;
-	nlohmann::json GetLayerData(int layerId) const;
+	static constexpr float Scale = 2.0f;
 
+public:
 	sf::Vector2i GetDimensions() const;
-	sf::Vector2i GetTileAmount() const;
-	sf::Vector2i GetTileSize() const;
-
 
 private:
-	void PopulateSpritePositions();
-	void ParseFileToJson(const std::string& filename);
+	sf::Vector2i mapTiles;
+	sf::Vector2i tileDimensions;
 
-
-
-private:
-	std::vector<sf::Texture> textures;
-	std::vector<sf::Sprite> sprites;
-
-	nlohmann::json jsonData;
-
-	int width;
-	int height;
-	int layers;
-
-	const sf::Texture& texture;
-	std::vector<sf::Vector2i> spritePositions;
+	std::vector<Tile> tiles;
 };
