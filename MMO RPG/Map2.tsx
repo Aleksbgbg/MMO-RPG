636ed90308f7dd852581087d33454cbb04ecf506
Aleksbgbg <kozichka01@abv.bg>
2018-06-27 22:09:42 +0100

    [Map] Add second map, and add portal at the top of first map.

--- MMO RPG/Map2.tsx
+++ MMO RPG/Map2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="mapTest" tilewidth="16" tileheight="16" tilecount="462" columns="22">
+<tileset name="Map2" tilewidth="16" tileheight="16" tilecount="462" columns="22">
  <image source="Textures/Map.png" width="352" height="336"/>
 </tileset>
