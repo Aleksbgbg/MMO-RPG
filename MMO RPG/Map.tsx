0502c861de1658a295defd69eeaf921b6b4e3a0f
Aleksbgbg <kozichka01@abv.bg>
2018-06-27 17:03:45 +0100

    [Map Resources] Rename 'mapTest' files to 'Map', and move to 'Map' filter (and out of own folder).

--- MMO RPG/Map.tsx
+++ MMO RPG/Map.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="mapTest" tilewidth="16" tileheight="16" tilecount="462" columns="22">
- <image source="mmo-rpg/MMO RPG/Textures/mapTest.png" width="352" height="336"/>
+ <image source="Textures/mapTest.png" width="352" height="336"/>
 </tileset>
