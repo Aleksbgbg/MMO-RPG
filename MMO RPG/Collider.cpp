2a1b64d1c51cdb0261b6ca70e6ff36ccbe578e59
Aleksbgbg <kozichka01@abv.bg>
2018-06-29 22:09:31 +0100

    [Collider] Add 'is_inside' function, to check if a rectangle is within another.

--- MMO RPG/Collider.cpp
+++ MMO RPG/Collider.cpp
@@ -11,3 +11,9 @@ bool are_overlapping(const sf::FloatRect& first, const sf::FloatRect& second)
 	return first.left <= secondRight && second.left <= firstRight &&
 		   first.top <= secondBottom && second.top <= firstBottom;
 }
+
+bool is_inside(const sf::FloatRect& bounds, const sf::FloatRect& source)
+{
+	return bounds.left <= source.left && source.left + source.width <= bounds.left + bounds.width &&
+		   bounds.top <= source.top && source.top + source.height <= bounds.top + bounds.height;
+}
--- MMO RPG/Collider.h
+++ MMO RPG/Collider.h
@@ -3,3 +3,5 @@
 #include <SFML/Graphics/Rect.hpp>
 
 bool are_overlapping(const sf::FloatRect& first, const sf::FloatRect& second);
+
+bool is_inside(const sf::FloatRect& bounds, const sf::FloatRect& source);
