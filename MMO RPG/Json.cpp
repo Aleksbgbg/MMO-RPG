3453b4b29cf245f8c1cd64fd769514385d5cd079
Aleksbgbg <kozichka01@abv.bg>
2018-06-30 22:55:32 +0100

    [Json] Add exception when file cannot be opened.

--- MMO RPG/Json.cpp
+++ MMO RPG/Json.cpp
@@ -8,5 +8,10 @@ json read_json(const std::string& filename)
 {
 	std::ifstream input{ filename };
 
+	if (!input.is_open())
+	{
+		throw std::runtime_error{ "Could not open JSON file." };
+	}
+
 	return json::parse(input);
 }
