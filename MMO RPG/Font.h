c241d4a20d53a8a125d87a81b7598c72f6af0382
Aleksbgbg <kozichka01@abv.bg>
2018-07-01 20:19:19 +0100

    [ResourceManagement] Add two static structs to hold data about different resources (currently the folders which hold them).

---
+++ MMO RPG/Font.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <string>
+
+namespace ResourceManagement
+{
+	struct Font
+	{
+		inline static std::string Folder = "Fonts";
+	};
+}
---
+++ MMO RPG/Texture.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <string>
+
+namespace ResourceManagement
+{
+	struct Texture
+	{
+		inline static std::string Folder = "Textures";
+	};
+}
