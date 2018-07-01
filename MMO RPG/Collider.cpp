02e21c1663f67674cdded4c19c4b6a669c16f9b3
Aleksbgbg <kozichka01@abv.bg>
2018-07-01 10:42:20 +0100

    [Collider] Remove overlapping function since this is already provided for rectangles.

--- MMO RPG/Collider.cpp
+++ MMO RPG/Collider.cpp
@@ -1,17 +1,5 @@
 #include "Collider.h"
 
-bool are_overlapping(const sf::FloatRect& first, const sf::FloatRect& second)
-{
-	const int firstRight = first.left + first.width;
-	const int firstBottom = first.top + first.height;
-
-	const int secondRight = second.left + second.width;
-	const int secondBottom = second.top + second.height;
-
-	return first.left <= secondRight && second.left <= firstRight &&
-		   first.top <= secondBottom && second.top <= firstBottom;
-}
-
 bool is_inside(const sf::FloatRect& bounds, const sf::FloatRect& source)
 {
 	return bounds.left <= source.left && source.left + source.width <= bounds.left + bounds.width &&
--- MMO RPG/Collider.h
+++ MMO RPG/Collider.h
@@ -2,6 +2,4 @@
 
 #include <SFML/Graphics/Rect.hpp>
 
-bool are_overlapping(const sf::FloatRect& first, const sf::FloatRect& second);
-
 bool is_inside(const sf::FloatRect& bounds, const sf::FloatRect& source);
