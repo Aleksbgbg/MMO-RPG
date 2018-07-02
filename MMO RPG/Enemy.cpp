4bf287c9a1344c516c9222560f9de54869eeb675
Aleksbgbg <kozichka01@abv.bg>
2018-07-02 17:44:38 +0100

    [Enemy] Make enemy inherit directly from Character, since it may not share many similarities with NPC later.

--- MMO RPG/Enemy.cpp
+++ MMO RPG/Enemy.cpp
@@ -1,9 +1,13 @@
 #include "Enemy.h"
 
+#include "Random.h"
+
 using nlohmann::json;
 
 Enemy::Enemy(const sf::Texture& spriteSheet, const sf::Vector2i mapDimensions, const json& spriteConfig)
 	:
-	Npc{ spriteSheet, mapDimensions, spriteConfig }
+	Character{ sprite, spriteConfig, movementStrategy },
+	sprite{ spriteSheet },
+	movementStrategy{ sprite, Random::GenerateFloat(1.0f, 2.0f), mapDimensions }
 {
 }
--- MMO RPG/Enemy.h
+++ MMO RPG/Enemy.h
@@ -1,9 +1,17 @@
 #pragma once
 
-#include "Npc.h"
+#include "Animation.h"
+#include "Character.h"
+#include "Map.h"
+#include "SimpleAiMovement.h"
 
-class Enemy : public Npc
+class Enemy : public Character
 {
 public:
 	Enemy(const sf::Texture& spriteSheet, const sf::Vector2i mapDimensions, const nlohmann::json& spriteConfig);
+
+private:
+	sf::Sprite sprite;
+
+	SimpleAiMovement movementStrategy;
 };
