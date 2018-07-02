681f836e627fae2d09af55d1fefd7b7e9e2cb056
Aleksbgbg <kozichka01@abv.bg>
2018-07-02 17:21:53 +0100

    [Npc] Remove animation setup (since done in Character).

--- MMO RPG/Npc.cpp
+++ MMO RPG/Npc.cpp
@@ -3,47 +3,14 @@
 #include "Random.h"
 #include "ResourceManager.h"
 #include "Map.h"
+#include "Json.h"
 
 using nlohmann::json;
 
-Npc::Npc(const sf::Vector2i spriteSheetCoordinate, const sf::Vector2i mapDimensions, const json& spriteConfig)
-	:
-	Npc{ spriteSheetCoordinate, TextureManager::Get("NPCs"), mapDimensions, spriteConfig }
-{
-}
-
 Npc::Npc(const sf::Texture& spriteSheet, const sf::Vector2i mapDimensions, const json& spriteConfig)
 	:
-	Character{ sprite, movementStrategy },
+	Character{ sprite, spriteConfig, movementStrategy },
 	sprite{ spriteSheet },
-	spriteInfo{ spriteConfig, spriteSheet },
 	movementStrategy{ sprite, Random::GenerateFloat(1.0f, 2.0f), mapDimensions }
 {
-	const sf::Vector2i spriteSheetDimension{ static_cast<int>(spriteInfo.frameCount * spriteInfo.spriteDimension.x), static_cast<int>(SpriteInfo::RowCount * spriteInfo.spriteDimension.y) };
-
-	const sf::IntRect spriteRegion = sf::IntRect{ sf::Vector2i{ 0, 0 }, spriteSheetDimension };
-
-	animations.emplace(Direction::Up, Animation{ sprite, spriteInfo, spriteInfo.upRow, spriteRegion });
-	animations.emplace(Direction::Down, Animation{ sprite, spriteInfo, spriteInfo.downRow, spriteRegion });
-	animations.emplace(Direction::Left, Animation{ sprite, spriteInfo, spriteInfo.leftRow, spriteRegion });
-	animations.emplace(Direction::Right, Animation{ sprite, spriteInfo, spriteInfo.rightRow, spriteRegion });
-}
-
-Npc::Npc(const sf::Vector2i spriteSheetCoordinate, const sf::Texture& spriteSheet, const sf::Vector2i mapDimensions, const json& spriteConfig)
-	:
-	Character{ sprite, movementStrategy },
-	sprite{ spriteSheet },
-	spriteInfo{ spriteConfig, spriteSheet },
-	movementStrategy{ sprite, Random::GenerateFloat(1.0f, 2.0f), mapDimensions }
-{
-	const sf::Vector2i spriteSheetDimension{ static_cast<int>(spriteInfo.frameCount * spriteInfo.spriteDimension.x), static_cast<int>(SpriteInfo::RowCount * spriteInfo.spriteDimension.y) };
-
-	const sf::IntRect spriteRegion = sf::IntRect{ spriteSheetCoordinate.x * spriteSheetDimension.x, spriteSheetCoordinate.y * spriteSheetDimension.y, spriteSheetDimension.x, spriteSheetDimension.y };
-
-	//sprite.setPosition(static_cast<float>(Random::Generate(0, mapDimensions.x - spriteInfo.spriteDimension.x)), static_cast<float>(Random::Generate(0, mapDimensions.y - spriteInfo.spriteDimension.y)));
-
-	animations.emplace(Direction::Up, Animation{ sprite, spriteInfo, spriteInfo.upRow, spriteRegion });
-	animations.emplace(Direction::Down, Animation{ sprite, spriteInfo, spriteInfo.downRow, spriteRegion });
-	animations.emplace(Direction::Left, Animation{ sprite, spriteInfo, spriteInfo.leftRow, spriteRegion });
-	animations.emplace(Direction::Right, Animation{ sprite, spriteInfo, spriteInfo.rightRow, spriteRegion });
 }
--- MMO RPG/Npc.h
+++ MMO RPG/Npc.h
@@ -8,18 +8,10 @@
 class Npc : public Character
 {
 public:
-	Npc(const sf::Vector2i spriteSheetCoordinate, const sf::Vector2i mapDimensions, const nlohmann::json& spriteConfig);
-
-protected:
 	Npc(const sf::Texture& spriteSheet, const sf::Vector2i mapDimensions, const nlohmann::json& spriteConfig);
 
 private:
-	Npc(sf::Vector2i spriteSheetCoordinate, const sf::Texture& spriteSheet, const sf::Vector2i mapDimensions, const nlohmann::json& spriteConfig);
-
-private:
 	sf::Sprite sprite;
-
-	SpriteInfo spriteInfo;
 
 	SimpleAiMovement movementStrategy;
 };
