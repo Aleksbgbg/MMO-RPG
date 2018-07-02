6111b6b54489f74bcc05766b9fe07378f4074825
Aleksbgbg <kozichka01@abv.bg>
2018-07-02 15:15:09 +0100

    [Vector] Ensure coordinates aren't 0 when normalizing.

--- MMO RPG/Vector.cpp
+++ MMO RPG/Vector.cpp
@@ -6,6 +6,13 @@ void normalize(sf::Vector2f& vector)
 {
 	const float hypotenuse = std::sqrt(vector.x * vector.x + vector.y * vector.y);
 
-	vector.x /= hypotenuse;
-	vector.y /= hypotenuse;
+	if (vector.x != 0)
+	{
+		vector.x /= hypotenuse;
+	}
+
+	if (vector.y != 0)
+	{
+		vector.y /= hypotenuse;
+	}
 }
