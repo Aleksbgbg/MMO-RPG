da5189e096640dbf0da043ec15364e0146ad74d2
Aleksbgbg <kozichka01@abv.bg>
2018-07-02 15:17:11 +0100

    [Npc] Implement SimpleAiMovement strategy.

--- MMO RPG/Npc.cpp
+++ MMO RPG/Npc.cpp
@@ -14,14 +14,10 @@ Npc::Npc(const sf::Vector2i spriteSheetCoordinate, const sf::Vector2i mapDimensi
 
 Npc::Npc(const sf::Texture& spriteSheet, const sf::Vector2i mapDimensions, const json& spriteConfig)
 	:
-	Character{ sf::Sprite{ spriteSheet }, Random::GenerateFloat(1.0f, 2.0f) },
-	mapDimensions{ mapDimensions },
-	spriteInfo{ spriteConfig, spriteSheet }
+	Character{ sf::Sprite{ spriteSheet }, movementStrategy },
+	spriteInfo{ spriteConfig, spriteSheet },
+	movementStrategy{ sprite, Random::GenerateFloat(1.0f, 2.0f), mapDimensions }
 {
-	GenerateTargetPosition();
-
-	sprite.setPosition(targetPosition);
-
 	const sf::Vector2i spriteSheetDimension{ static_cast<int>(spriteInfo.frameCount * spriteInfo.spriteDimension.x), static_cast<int>(SpriteInfo::RowCount * spriteInfo.spriteDimension.y) };
 
 	const sf::IntRect spriteRegion = sf::IntRect{ sf::Vector2i{ 0, 0 }, spriteSheetDimension };
@@ -34,83 +30,18 @@ Npc::Npc(const sf::Texture& spriteSheet, const sf::Vector2i mapDimensions, const
 
 Npc::Npc(const sf::Vector2i spriteSheetCoordinate, const sf::Texture& spriteSheet, const sf::Vector2i mapDimensions, const json& spriteConfig)
 	:
-	Character{ sf::Sprite{ spriteSheet }, Random::GenerateFloat(1.0f, 2.0f) },
-	mapDimensions{ mapDimensions },
-	spriteInfo{ spriteConfig, spriteSheet }
+	Character{ sf::Sprite{ spriteSheet }, movementStrategy },
+	spriteInfo{ spriteConfig, spriteSheet },
+	movementStrategy{ sprite, Random::GenerateFloat(1.0f, 2.0f), mapDimensions }
 {
 	const sf::Vector2i spriteSheetDimension{ static_cast<int>(spriteInfo.frameCount * spriteInfo.spriteDimension.x), static_cast<int>(SpriteInfo::RowCount * spriteInfo.spriteDimension.y) };
 
 	const sf::IntRect spriteRegion = sf::IntRect{ spriteSheetCoordinate.x * spriteSheetDimension.x, spriteSheetCoordinate.y * spriteSheetDimension.y, spriteSheetDimension.x, spriteSheetDimension.y };
 
-	GenerateTargetPosition();
-
-	sprite.setPosition(targetPosition);
+	//sprite.setPosition(static_cast<float>(Random::Generate(0, mapDimensions.x - spriteInfo.spriteDimension.x)), static_cast<float>(Random::Generate(0, mapDimensions.y - spriteInfo.spriteDimension.y)));
 
 	animations.emplace(Direction::Up, Animation{ sprite, spriteInfo, spriteInfo.upRow, spriteRegion });
 	animations.emplace(Direction::Down, Animation{ sprite, spriteInfo, spriteInfo.downRow, spriteRegion });
 	animations.emplace(Direction::Left, Animation{ sprite, spriteInfo, spriteInfo.leftRow, spriteRegion });
 	animations.emplace(Direction::Right, Animation{ sprite, spriteInfo, spriteInfo.rightRow, spriteRegion });
-}
-
-sf::Vector2f Npc::PickMovement()
-{
-	const sf::Vector2f currentPosition{ sprite.getPosition() };
-
-	sf::Vector2f movement;
-
-	if (currentPosition.x != targetPosition.x) // Move towards target x first
-	{
-		if (currentPosition.x > targetPosition.x)
-		{
-			movement.x = -1;
-		}
-		else
-		{
-			movement.x = 1;
-		}
-	}
-	else // Move towards target y, after reaching target x
-	{
-		if (currentPosition.y > targetPosition.y)
-		{
-			movement.y = -1;
-		}
-		else
-		{
-			movement.y = 1;
-		}
-	}
-
-	return movement;
-}
-
-void Npc::OnPositionUpdated(const sf::Vector2f newPosition)
-{
-	if (newPosition == targetPosition)
-	{
-		GenerateTargetPosition();
-		return;
-	}
-
-	const sf::Vector2f currentVector = newPosition - targetPosition;
-
-	if (!(currentVector.x == 0 || (startingTargetVector.x >= 0 ^ currentVector.x < 0)))
-	{
-		SetPosition(sf::Vector2f{ targetPosition.x, sprite.getPosition().y });
-	}
-
-	if (!(currentVector.y == 0 || (startingTargetVector.y >= 0 ^ currentVector.y < 0)))
-	{
-		SetPosition(sf::Vector2f{ sprite.getPosition().x, targetPosition.y });
-	}
-}
-
-void Npc::GenerateTargetPosition()
-{
-	// Consider checking if the NPC is set a target position already equal to the current position, and choose a new target if so (may not be necessary)
-
-	targetPosition.x = static_cast<float>(Random::Generate(0, mapDimensions.x - spriteInfo.spriteDimension.x));
-	targetPosition.y = static_cast<float>(Random::Generate(0, mapDimensions.y - spriteInfo.spriteDimension.y));
-
-	startingTargetVector = sprite.getPosition() - targetPosition;
 }
--- MMO RPG/Npc.h
+++ MMO RPG/Npc.h
@@ -3,6 +3,7 @@
 #include "Animation.h"
 #include "Character.h"
 #include "Map.h"
+#include "SimpleAiMovement.h"
 
 class Npc : public Character
 {
@@ -12,23 +13,11 @@ public:
 protected:
 	Npc(const sf::Texture& spriteSheet, const sf::Vector2i mapDimensions, const nlohmann::json& spriteConfig);
 
-protected:
-	 sf::Vector2f PickMovement() override;
-	 
-	 void OnPositionUpdated(const sf::Vector2f newPosition) override;
-
 private:
 	Npc(sf::Vector2i spriteSheetCoordinate, const sf::Texture& spriteSheet, const sf::Vector2i mapDimensions, const nlohmann::json& spriteConfig);
 
 private:
-	void GenerateTargetPosition();
-
-private:
-	const sf::Vector2i mapDimensions;
-
 	SpriteInfo spriteInfo;
 
-	sf::Vector2f targetPosition;
-
-	sf::Vector2f startingTargetVector;
+	SimpleAiMovement movementStrategy;
 };
