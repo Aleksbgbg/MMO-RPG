be86418a3924937aabf8a499e14ea01637a195b1
Aleksbgbg <kozichka01@abv.bg>
2018-07-02 17:52:52 +0100

    [Enemy] Add health and healthBar.

--- MMO RPG/Enemy.cpp
+++ MMO RPG/Enemy.cpp
@@ -8,6 +8,15 @@ Enemy::Enemy(const sf::Texture& spriteSheet, const sf::Vector2i mapDimensions, c
 	:
 	Character{ sprite, spriteConfig, movementStrategy },
 	sprite{ spriteSheet },
-	movementStrategy{ sprite, Random::GenerateFloat(1.0f, 2.0f), mapDimensions }
+	movementStrategy{ sprite, Random::GenerateFloat(1.0f, 2.0f), mapDimensions },
+	health{ 100.0f },
+	healthBar{ sf::Color::Green }
 {
+}
+
+void Enemy::OnDraw(const Graphics& gfx)
+{
+	const sf::FloatRect spriteBounds = sprite.getLocalBounds();
+
+	healthBar.Render(gfx, health / MaxHealth, sprite.getPosition() + sf::Vector2f{ spriteBounds.width / 2.0f, -10.0f });
 }
--- MMO RPG/Enemy.h
+++ MMO RPG/Enemy.h
@@ -4,14 +4,23 @@
 #include "Character.h"
 #include "Map.h"
 #include "SimpleAiMovement.h"
+#include "StatBar.h"
 
 class Enemy : public Character
 {
 public:
 	Enemy(const sf::Texture& spriteSheet, const sf::Vector2i mapDimensions, const nlohmann::json& spriteConfig);
 
+protected:
+	void OnDraw(const Graphics& gfx) override;
+
+private:
+	static constexpr float MaxHealth = 100.0f;
+
 private:
 	sf::Sprite sprite;
-
 	SimpleAiMovement movementStrategy;
+
+	float health;
+	StatBar healthBar;
 };
