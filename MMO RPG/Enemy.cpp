368e4968d804b71c57126b52d6ae25969334bc35
Aleksbgbg <kozichka01@abv.bg>
2018-07-04 19:37:23 +0100

    [Enemy] Inherit InteractiveCharacter for health implementation.

--- MMO RPG/Enemy.cpp
+++ MMO RPG/Enemy.cpp
@@ -6,17 +6,8 @@ using nlohmann::json;
 
 Enemy::Enemy(const sf::Texture& spriteSheet, const sf::Vector2i mapDimensions, const json& spriteConfig)
 	:
-	Character{ sprite, spriteConfig, movementStrategy },
+	InteractiveCharacter{ sprite, spriteConfig, movementStrategy },
 	sprite{ spriteSheet },
-	movementStrategy{ sprite, Random::GenerateFloat(1.0f, 2.0f), mapDimensions },
-	health{ 100.0f },
-	healthBar{ sf::Color::Green }
+	movementStrategy{ sprite, Random::GenerateFloat(1.0f, 2.0f), mapDimensions }
 {
-}
-
-void Enemy::OnDraw(const Graphics& gfx)
-{
-	const sf::FloatRect spriteBounds = sprite.getLocalBounds();
-
-	healthBar.Render(gfx, health / MaxHealth, sprite.getPosition() + sf::Vector2f{ spriteBounds.width / 2.0f, -10.0f });
 }
--- MMO RPG/Enemy.h
+++ MMO RPG/Enemy.h
@@ -1,26 +1,16 @@
 #pragma once
 
 #include "Animation.h"
-#include "Character.h"
 #include "Map.h"
 #include "SimpleAiMovement.h"
-#include "StatBar.h"
+#include "InteractiveCharacter.h"
 
-class Enemy : public Character
+class Enemy : public InteractiveCharacter
 {
 public:
 	Enemy(const sf::Texture& spriteSheet, const sf::Vector2i mapDimensions, const nlohmann::json& spriteConfig);
 
-protected:
-	void OnDraw(const Graphics& gfx) override;
-
-private:
-	static constexpr float MaxHealth = 100.0f;
-
 private:
 	sf::Sprite sprite;
 	SimpleAiMovement movementStrategy;
-
-	float health;
-	StatBar healthBar;
 };
