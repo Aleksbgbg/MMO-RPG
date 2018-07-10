9df09d3338469b8aacd44876341a1b0bf76f4398
Aleksbgbg <kozichka01@abv.bg>
2018-07-10 19:35:26 +0100

    [ResourceManagement] Add file extensions for resource management template classes.

--- MMO RPG/Font.h
+++ MMO RPG/Font.h
@@ -6,5 +6,6 @@ namespace ResourceManagement
 	struct Font
 	{
 		inline static std::string Folder = "Fonts";
+		inline static std::string Extension = ".ttf";
 	};
 }
--- MMO RPG/Texture.h
+++ MMO RPG/Texture.h
@@ -6,5 +6,6 @@ namespace ResourceManagement
 	struct Texture
 	{
 		inline static std::string Folder = "Textures";
+		inline static std::string Extension = ".png";
 	};
 }
