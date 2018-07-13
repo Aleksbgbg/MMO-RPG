db57ae71d2b8992ef3eb935f911673ab4c3d1c27
Aleksbgbg <kozichka01@abv.bg>
2018-07-13 17:42:26 +0100

    [HudWindow] Implement toggle open key, and only call virtual methods on HudWindows which are opened. Implement virtual OnDraw, OnKeyPressed, and OnMouseClicked methods.

--- MMO RPG/HudWindow.cpp
+++ MMO RPG/HudWindow.cpp
@@ -1,7 +1,8 @@
 #include "HudWindow.h"
 
-HudWindow::HudWindow()
+HudWindow::HudWindow(const sf::Keyboard::Key toggleOpenKey)
 	:
+	toggleOpenKey{ toggleOpenKey },
 	opened{ false }
 {
 }
@@ -9,4 +10,45 @@ HudWindow::HudWindow()
 void HudWindow::ToggleOpened()
 {
 	opened = !opened;
+}
+
+void HudWindow::Draw(const Graphics& gfx)
+{
+	if (!opened)
+	{
+		OnDraw(gfx);
+	}
+}
+
+void HudWindow::KeyPressed(const sf::Keyboard::Key key)
+{
+	if (key == toggleOpenKey)
+	{
+		ToggleOpened();
+	}
+
+	if (opened)
+	{
+		OnKeyPressed(key);
+	}
+}
+
+void HudWindow::MouseClicked(const sf::Vector2f position)
+{
+	if (opened)
+	{
+		OnMouseClicked(position);
+	}
+}
+
+void HudWindow::OnDraw(const Graphics& gfx)
+{
+}
+
+void HudWindow::OnKeyPressed(const sf::Keyboard::Key key)
+{
+}
+
+void HudWindow::OnMouseClicked(const sf::Vector2f position)
+{
 }
--- MMO RPG/HudWindow.h
+++ MMO RPG/HudWindow.h
@@ -1,16 +1,34 @@
 #pragma once
 
+#include <SFML/Window/Keyboard.hpp>
+#include <SFML/System/Vector2.hpp>
+
+#include "Graphics.h"
+
 class HudWindow
 {
 public:
-	HudWindow();
+	explicit HudWindow(const sf::Keyboard::Key toggleOpenKey);
 
 public:
 	virtual ~HudWindow() = default;
 
-public:
+	void Draw(const Graphics& gfx);
+
+	void KeyPressed(const sf::Keyboard::Key key);
+	void MouseClicked(const sf::Vector2f position);
+
+protected:
+	virtual void OnDraw(const Graphics& gfx);
+
+	virtual void OnKeyPressed(const sf::Keyboard::Key key);
+	virtual void OnMouseClicked(const sf::Vector2f position);
+
+private:
 	void ToggleOpened();
 
 private:
+	sf::Keyboard::Key toggleOpenKey;
+
 	bool opened;
 };
