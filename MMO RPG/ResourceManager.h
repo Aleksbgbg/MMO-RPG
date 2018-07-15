439ad1f05ba1e583b10894392326bd2afad25053
Aleksbgbg <kozichka01@abv.bg>
2018-07-15 20:30:33 +0100

    [ResourceManager] Move implementation to header file due to templates.

--- MMO RPG/ResourceManager.h
+++ MMO RPG/ResourceManager.h
@@ -3,6 +3,7 @@
 #include <SFML/Graphics/Texture.hpp>
 #include <SFML/Graphics/Font.hpp>
 
+#include <filesystem>
 #include <unordered_map>
 
 #include "Texture.h"
@@ -12,10 +13,36 @@ template<typename T, typename TResourceType>
 class ResourceManager
 {
 public:
-	static const T& Get(const std::string& name);
+	static const T& Get(const std::string& name)
+	{
+		std::unordered_map<std::string, T>& resourceMap = GetResourceMap();
+
+		const auto iterator = resourceMap.find(name);
+
+		if (iterator == resourceMap.end())
+		{
+			const std::string filename = std::filesystem::current_path().string() + "\\" + TResourceType::Folder + "\\" + name + TResourceType::Extension;
+
+			T resource;
+
+			if (!resource.loadFromFile(filename))
+			{
+				throw std::runtime_error{ "Could not load resource " + name };
+			}
+
+			return resourceMap.emplace(filename.substr(0, filename.find_last_of('.')), resource).first->second;
+		}
+
+		return iterator->second;
+	}
 
 private:
-	static std::unordered_map<std::string, T> resourceMap;
+	static std::unordered_map<std::string, T>& GetResourceMap()
+	{
+		static std::unordered_map<std::string, T> resourceMap;
+
+		return resourceMap;
+	}
 };
 
 typedef ResourceManager<sf::Texture, ResourceManagement::Texture> TextureManager;
