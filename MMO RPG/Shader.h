1b8b534eac147efea16baaddee5e733520c3b66f
Aleksbgbg <kozichka01@abv.bg>
2018-07-15 21:26:11 +0100

    [ResourceManagement] Add 'Shader' static info struct.

---
+++ MMO RPG/Shader.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+
+namespace ResourceManagement
+{
+	struct Shader
+	{
+		inline static std::string Folder = "Shaders";
+		inline static std::string Extension = ".glsl";
+	};
+}
