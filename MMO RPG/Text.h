95700a1a920f99f69cbd68d6d9ec8752d61e5b6b
Aleksbgbg <kozichka01@abv.bg>
2018-07-15 21:12:49 +0100

    [Text] Add default constructor.

--- MMO RPG/Text.h
+++ MMO RPG/Text.h
@@ -5,6 +5,7 @@
 class Text : public sf::Text
 {
 public:
+	Text() = default;
 	Text(const sf::String& string, const std::shared_ptr<sf::Font>& font, const unsigned int characterSize = 30);
 
 private:
