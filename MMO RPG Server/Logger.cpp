ba23b10334fee075ba128ceaf05ca57ca2cb109a
Aleksbgbg <kozichka01@abv.bg>
2018-08-04 13:04:20 +0100

    [MMO RPG Server] Move logging function to logger files.

---
+++ MMO RPG Server/Logger.cpp
@@ -0,0 +1,14 @@
+#include "Logger.h"
+
+#include <iostream>
+#include <iomanip>
+
+void Log(const sf::TcpSocket& playerSocket, const std::string& message)
+{
+	const auto time = std::time(nullptr);
+
+	std::cout << "[" << std::put_time(std::localtime(&time), "%d/%m/%Y %H:%M:%S") << "] "
+		<< "(" << playerSocket.getRemoteAddress() << ":" << playerSocket.getRemotePort() << ") "
+		<< message
+		<< "\n";
+}
---
+++ MMO RPG Server/Logger.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <SFML/Network.hpp>
+
+void Log(const sf::TcpSocket& playerSocket, const std::string& message);
